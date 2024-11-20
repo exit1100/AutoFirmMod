e56afc60ee8754fecc786d3a9000697066642f8b
exit1100 <exit3015@gmail.com>
2024-11-20 11:06:15 +0900

    fix : consider '/usr' path mount for existing telnetd backdoor

--- llm.py
+++ llm.py
@@ -34,11 +34,12 @@ def target_file_path_prompt(setting_path, binary_path, user_dir) :
         "정보:\n"
         f"settings_path = {setting_path}\n"
         f"binary_path = {binary_path}\n\n"
-        "질문: 정보를 참고하여 부팅 스크립트에 telnetd를 백그라운드로 실행하는 명령어를 echo로 추가해주시고, shadow 파일의 root 계정 비밀번호를 sed -i 's/^root:[^:]*:/root::/' 명령어로 제거해주세요."
+        "질문: 정보를 참고하여 부팅 스크립트에 telnetd를 백그라운드로 실행하는 명령어를 echo로 추가해주시고, telnetd는 /usr 경로에 마운트 될 수 있으므로 둘 다 추가해주세요.\n"
+        "shadow 파일의 root 계정 비밀번호를 sed -i 's/^root:[^:]*:/root::/' 명령어로 제거해주세요."
         "파이썬 리스트 형태로 답변해주시고, 대부분의 경로를 생략하지마세요."
         "telnetd 는 마운트 된 이후 경로를 써야하므로 앞에 파일 시스템 경로는 생략해주세요."
         f"값을 쓰는 파일의 경로는 앞에 {user_dir}/ 경로를 추가해야 합니다."
-        "그 외의 문구나 코드 블록(```)을 포함하지 마세요."
+        "그 외의 문구나 코드 블록(```)을 반드시 포함하지 마세요."
         ""
     )
     return system_prompt
